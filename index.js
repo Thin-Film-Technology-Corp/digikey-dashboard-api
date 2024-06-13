d22bd22a8046c681a6d7260bff05b871469909b6
jordan-carlson99 <jcarlson@thin-film.com>
2024-06-13 15:02:59 -0500

    added isAuthorizing check

--- index.js
+++ index.js
@@ -34,10 +34,13 @@ const authorize = (req, res, next) => {
 
 // In-memory storage for session credentials
 let sessionObj = null;
+let isAuthorizing = false;
 
 const getSessionCredentials = async () => {
+  isAuthorizing = true;
   console.log("Fetching new session credentials...");
   sessionObj = await getDigiKeyMicroStrategySession();
+  isAuthorizing = false;
   return sessionObj;
 };
 
@@ -54,8 +57,13 @@ app.get("/csv/:document", authorize, async (req, res) => {
   const getCsvData = async () => {
     try {
       console.log("Retrieving session information...");
-      if (!sessionObj) {
+      if (!sessionObj && !isAuthorizing) {
         sessionObj = await getSessionCredentials();
+      } else if (!sessionObj && isAuthorizing) {
+        console.log(`Recieved request while authorizing!`);
+        return res
+          .status(503)
+          .end("Please wait for authorization before attempting again");
       }
       console.log("Using session information...");
 
@@ -76,11 +84,16 @@ app.get("/csv/:document", authorize, async (req, res) => {
       res.status(200).send(csv).end();
     } catch (error) {
       console.log(`Error getting CSVs: ${error} \n${error.stack}`);
-      if (error.statusCode === 401 && retries < 2) {
+      if (error.statusCode === 401 && retries < 2 && !isAuthorizing) {
         retries++;
         console.log("Session expired. Fetching new session credentials...");
         sessionObj = await getSessionCredentials();
         return getCsvData(); // Retry with new session credentials
+      } else if (error.statusCode === 401 && retries < 2 && isAuthorizing) {
+        console.log(`Recieved request while authorizing!`);
+        return res
+          .status(503)
+          .end("Please wait for authorization before attempting again");
       } else {
         res.status(500).end("Internal error!");
       }

b51610460061b2d7e38b540c49caaf9c3f4ad7d4
jordan-carlson99 <jcarlson@thin-film.com>
2024-08-28 15:56:51 -0500

    Added worker threads to competitor sync

--- competitor_syncing/competitorSync.js
+++ competitor_syncing/competitorSync.js
@@ -5,6 +5,17 @@ import {
   getAllPartsInDigikeySearchV4,
 } from "../digiKeyAPI.js";
 import { createHash } from "node:crypto";
+import {
+  isMainThread,
+  Worker,
+  parentPort,
+  workerData,
+} from "node:worker_threads";
+import { readFileSync, writeFileSync } from "fs";
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
 
 function logExceptOnTest(string) {
   if (process.env.NODE_ENV !== "test") {
@@ -74,11 +85,12 @@ export async function retrieveResistorPNs(accessToken, body) {
   let total;
   let promiseArray = [];
   let markers = [];
+  let redos = [];
 
   body = body || {
     Keywords: "Resistor",
     Limit: 50,
-    Offset: 121800,
+    Offset: 115200,
     FilterOptionsRequest: {
       ManufacturerFilter: [],
       MinimumQuantityAvailable: 1,
@@ -148,22 +160,36 @@ export async function retrieveResistorPNs(accessToken, body) {
   const pns = [];
   promiseArray = await Promise.all(
     promiseArray.map((r) =>
-      r.data.then((res) =>
-        res.json().then((d) => {
+      r.data
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error(
+              `promise for part numbering failed!\n${res.status}`
+            );
+          }
+          return res.json();
+        })
+        .then((d) => {
           pns.push(...d.Products);
           return { ...d, index: r.index };
         })
-      )
+        .catch((error) => {
+          console.error("Error processing request:", error);
+          // Return a default value or handle the error in a way that doesn't break the Promise.all
+          // return { error: true, index: r.index, message: error.message };
+        })
     )
   );
-
   // Validate that we got all of our information
   // Check if length matches number of batches
   if (pns.length !== Math.round(numberOfBatches * 50)) {
     logExceptOnTest(`pns require validation`);
-    let redos = validatePNs(markers, initialOffset, total, body.Limit);
+    let validatedRedos = validatePNs(markers, initialOffset, total, body.Limit);
+    redos.push(...validatedRedos);
+    logExceptOnTest(`${redos.length} batches require another attempt`);
     let additionalPNs = await remediatePNs(redos, body, accessToken);
     pns.push(...additionalPNs);
+    logExceptOnTest(`pushed ${additionalPNs.length} redone parts into pns`);
   }
 
   return pns.map(structurePNs);
@@ -171,14 +197,14 @@ export async function retrieveResistorPNs(accessToken, body) {
 
 function addHashAndDate(data, hash = false) {
   const date = new Date();
-  data.day = date.getDate();
-  data.month = date.getMonth() + 1;
-  data.year = date.getFullYear();
 
   if (hash) {
     const hashInstance = createHash("MD5");
     hashInstance.update(JSON.stringify(data));
     data.hash = hashInstance.digest("hex");
+    data.day = date.getDate();
+    data.month = date.getMonth() + 1;
+    data.year = date.getFullYear();
   }
 
   return data;
@@ -256,7 +282,22 @@ function compareHashes(newData, oldData) {
   return oldData;
 }
 
-export async function compareQueryToDatabase(queryResults, database) {
+// divides the database comparisons between the core count
+// spawns worker threads
+// Distributes the comparisons between the worker threads by adding the comparison data to the thread
+export async function compareQueryToDatabase(
+  queryResults,
+  database,
+  coreCount
+) {
+  let testFile =
+    (await readFileSync("./temp/results.csv").toString()) ||
+    '"core count","elapsed time","number of parts","parts / ms"';
+
+  const startTime = Date.now();
+  coreCount = coreCount || 1;
+
+  // get all the part numbers from the queryresults and request all the parts from the database
   const partNumbers = queryResults.map((pn) => pn.part_number);
   const existingParts = await database
     .find({ part_number: { $in: partNumbers } })
@@ -264,13 +305,78 @@ export async function compareQueryToDatabase(queryResults, database) {
 
   const bulkOp = [];
   const insertionList = [];
+
+  // create a Map of the parts with the key being the part number
   const existingPartsMap = new Map(
     existingParts.map((p) => [p.part_number, p])
   );
 
+  const workerPNIncrement = Math.ceil(queryResults.length / coreCount);
+  let workerPNIndex = 0;
+
+  let promises = [];
+
+  for (let i = 0; i < coreCount; i++) {
+    let partSubset = queryResults.slice(
+      workerPNIndex,
+      workerPNIndex + workerPNIncrement
+    );
+    workerPNIndex += workerPNIncrement;
+
+    if (partSubset.length > 0) {
+      const workerPromise = new Promise((resolve, reject) => {
+        const worker = new Worker(__filename, {
+          workerData: {
+            partNumbers: partSubset,
+            existingPartsMap: Array.from(existingPartsMap.entries()),
+          },
+        });
+
+        worker.on("message", resolve);
+        worker.on("error", reject);
+        worker.on("exit", (code) => {
+          if (code !== 0) {
+            reject(new Error(`Worker stopped with exit code ${code}`));
+          }
+        });
+      });
+
+      promises.push(workerPromise);
+    }
+  }
+  const results = await Promise.all(promises);
+
+  results.forEach(
+    ({ bulkOp: workerBulkOp, insertionList: workerInsertionList }) => {
+      bulkOp.push(...workerBulkOp);
+      insertionList.push(...workerInsertionList);
+    }
+  );
+  const endTime = Date.now();
+  const elapsedTime = endTime - startTime;
+  logExceptOnTest(
+    `time for comparison execution: ${elapsedTime} ms for ${
+      partNumbers.length
+    } parts\n${partNumbers.length / elapsedTime} parts / ms`
+  );
+  writeFileSync(
+    "./temp/results.csv",
+    (testFile += `\n${coreCount},${elapsedTime},${partNumbers.length},${
+      partNumbers.length / elapsedTime
+    }`)
+  );
+
+  return { bulkOp, insertionList };
+}
+
+// This is what a worker thread will execute to compare
+function processPartNumbers(queryResults, existingPartsMap) {
+  const bulkOp = [];
+  const insertionList = [];
   queryResults.forEach((pn) => {
     const oldPNData = existingPartsMap.get(pn.part_number);
     if (oldPNData) {
+      // add to
       const combinedPricing = compareHashes(pn.pricing, oldPNData.pricing);
       const combinedInventory = compareHashes(
         pn.inventory,
@@ -288,49 +394,37 @@ export async function compareQueryToDatabase(queryResults, database) {
       insertionList.push(pn);
     }
   });
-
   return { bulkOp, insertionList };
 }
 
-export async function syncCompetitors() {
-  logExceptOnTest("getting access token for digikey...");
-  const accessToken = await getAccessTokenForDigikeyAPI();
+// Abstraction that calls the functions in their order
+// Connects to mongoDB and makes the additions
+export async function syncCompetitors(numSyncs) {
+  numSyncs = numSyncs || 1;
+  for (let i = 0; i < numSyncs; i++) {
+    logExceptOnTest("getting access token for digikey...");
+    const accessToken = await getAccessTokenForDigikeyAPI();
 
-  logExceptOnTest("retrieving all Chip Resistors from Digikey...");
-  const pns = await retrieveResistorPNs(accessToken);
+    logExceptOnTest("retrieving all Chip Resistors from Digikey...");
+    const pns = await retrieveResistorPNs(accessToken);
 
-  // console.log(pns.length);
+    // console.log(pns.length);
 
-  logExceptOnTest("connecting to Mongo instance...");
-  const client = new MongoClient(
-    process.env.competitor_database_connection_string
-  );
-  await client.connect();
-  const db = client.db("CompetitorDBInstance");
-  const dkChipResistor = db.collection("dk_chip_resistor");
+    logExceptOnTest("connecting to Mongo instance...");
+    const client = new MongoClient(
+      process.env.competitor_database_connection_string
+    );
+    await client.connect();
+    const db = client.db("CompetitorDBInstance");
+    const dkChipResistor = db.collection("dk_chip_resistor");
 
-  logExceptOnTest("comparing delta between query and Mongo...");
-  const operations = await compareQueryToDatabase(pns, dkChipResistor);
+    logExceptOnTest("comparing delta between query and Mongo...");
+    const operations = await compareQueryToDatabase(pns, dkChipResistor, 1);
 
-  // if (operations.insertionList.length > 0) {
-  //   await dkChipResistor.insertMany(operations.insertionList);
-  // }
-
-  // if (operations.bulkOp.length > 0) {
-  //   await dkChipResistor.bulkWrite(operations.bulkOp);
-  // }
-
-  // logExceptOnTest(
-  //   `completed:\n\t${operations.bulkOp.length} doc(s) updated\n\t${operations.insertionList.length} doc(s) created`
-  // );
-
-  logExceptOnTest("closing client...");
-  await client.close();
+    logExceptOnTest("closing client...");
+    await client.close();
+  }
 }
-
-syncCompetitors().then((data) => {
-  // logExceptOnTest(data)
-});
 
 async function findDuplicatePartNumbers() {
   const client = new MongoClient(
@@ -351,4 +445,16 @@ async function findDuplicatePartNumbers() {
   await client.close();
 
   return duplicates;
+}
+
+if (!isMainThread) {
+  const { partNumbers, existingPartsMap } = workerData;
+  const existingPartsMapObj = new Map(existingPartsMap);
+  const result = processPartNumbers(partNumbers, existingPartsMapObj);
+  parentPort.postMessage(result);
+} else {
+  // TODO: handle 429 codes (30 second timeout)
+  syncCompetitors(1).then((data) => {
+    // logExceptOnTest(data)
+  });
 }
